aa501aa2fc4420a8082e3a1faaf8d8eae3150b8d
sarahdombroski <dom22003@byui.edu>
2025-05-14 11:47:48 -0600

    completed events ponder activity

--- w04_ponder/events.js
+++ w04_ponder/events.js
@@ -1,7 +1,11 @@
+// Sarah Dombroski
+
 function newTask() {
   // get the list element from the DOM
   // get the value entered into the #todo input
   // render out the list 
+  const listElement = document.querySelector('#todoList');
+  const task = document.querySelector('#todo').value;
   listElement.innerHTML += `
     <li> ${task}
       <div>
@@ -28,3 +32,10 @@ function manageTasks(e) {
 // Listen for a click, call the 'newTask' function on submit and 
 // call the 'manageTasks' function if either of the icons are 
 // clicked in the list of tasks.
+
+const submitButton = document.querySelector('#submitTask');
+submitButton.addEventListener('click', newTask);
+
+const complete = document.querySelector('#todoList');
+complete.addEventListener('click', manageTasks);
+
